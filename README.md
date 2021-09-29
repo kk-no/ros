096e343bad781542b5298a3db28ec6b916fdf992
kk-no <kenokohe@gmail.com>
2021-09-30 00:02:49 +0900

    add minimum boot image

---
+++ README.md
@@ -0,0 +1,8 @@
+# ros
+
+## commands
+
+```
+cargo check
+cargo build --target thumbv7em-none-eabihf
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // no link standad library.
+#![no_main] // disable rust level entry point.
+
+use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle] // no name
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
+}
+
+#[panic_handler] // call in case of panic.
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
